4e78f173d2f45a991abe07668b14c90cf83e93ce
Joao Rangel <joao.vran@gmail.com>
2020-10-29 17:46:22 -0300

    appointments migration fixes

--- src/database/migrations/1603837230772-CreateAppointments.ts
+++ src/database/migrations/1603837230772-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1603837230772
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
