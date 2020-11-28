d76f82eac4b4f3524be146199f1413c923d7a63b
Joao Rangel <joao.vran@gmail.com>
2020-11-28 18:30:14 -0300

    refactoring Appointment Repository

---
+++ src/modules/appointments/dtos/ICreateAppointmentDTO.ts
@@ -0,0 +1,4 @@
+export default interface ICreateAppointmenDTO {
+  provider_id: string;
+  date: Date;
+}
--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,7 @@
+import ICreateAppointmenDTO from '../dtos/ICreateAppointmentDTO';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 export default interface IAppointmentsRepository {
+  create(data: ICreateAppointmenDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
