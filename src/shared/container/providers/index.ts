710b8a70d4b49406f2244d274cd877a00c369473
Joao Rangel <joao.vran@gmail.com>
2020-12-04 22:08:09 -0300

    add mail template provider using handlebars

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -6,6 +6,9 @@ import DiskStorageProvider from './StorageProvider/Implementations/DiskStoragePr
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
