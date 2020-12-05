b1fa174b0a3ae44d178f1423065e09cba586bf84
Joao Rangel <joao.vran@gmail.com>
2020-12-04 22:20:48 -0300

    ajust send email params at recovery service

--- src/modules/users/services/SendRecoveryEmailService.ts
+++ src/modules/users/services/SendRecoveryEmailService.ts
@@ -30,10 +30,21 @@ class SendRecoveryEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
-    await this.mailProvider.sendMail(
-      email,
-      `Sua solicitação de recuperação de senha chegou! \n \n ${token}`,
-    );
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        template:
+          'Olá {{name}}, use o seguinte token para resetar sua senha: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 
